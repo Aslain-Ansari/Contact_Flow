dea114f8eecd478ac5bb7943b9f9260425561d42
Aslain Ansari <141625350+Aslain-Ansari@users.noreply.github.com>
2025-02-08 22:11:56 +0530

    fixed responsiveness in App.jsx and ContactCard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import ContactDetail from "./components/ContactDetail";
 import EditContact from "./components/EditContact";
 
 const App = () => {
-  // const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
@@ -21,7 +20,6 @@ const App = () => {
   };
 
   const addContactHandler = async (contact) => {
-    // console.log(contact);
     const request = {
       id: uuidv4(),
       ...contact,
@@ -32,7 +30,6 @@ const App = () => {
 
   const updateContactHandler = async (contact) => {
     const response = await api.put(`/contacts/${contact.id}`, contact);
-    // console.log(response.data);
     let { id } = response.data;
     setContacts(
       contacts.map((contact) => {
@@ -43,15 +40,11 @@ const App = () => {
 
   const removeContactHandler = async (id) => {
     await api.delete(`/contacts/${id}`);
-    // making copy of contacts
-    const newContactList = contacts.filter((contact) => {
-      return contact.id !== id;
-    });
+    const newContactList = contacts.filter((contact) => contact.id !== id);
     setContacts(newContactList);
   };
 
   const searchHandler = (searchTerm) => {
-    // console.log(searchTerm);
     setSearchTerm(searchTerm);
     if (searchTerm !== "") {
       const newContactList = contacts.filter((contact) => {
@@ -67,47 +60,51 @@ const App = () => {
   };
 
   useEffect(() => {
-    // const retrieveContacts = JSON.parse(
-    //   localStorage.getItem(LOCAL_STORAGE_KEY)
-    // );
-    // if (retrieveContacts) setContacts(retrieveContacts);
     const getAllContacts = async () => {
       const allContacts = await retrieveContacts();
       if (allContacts) setContacts(allContacts);
     };
     getAllContacts();
   }, []);
-  useEffect(() => {
-    //localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
-  }, [contacts]);
+
   return (
     <div className="ui container">
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ContactList
-                contacts={searchTerm.length < 1 ? contacts : searchResults}
-                getContactId={removeContactHandler}
-                term={searchTerm}
-                searchHandler={searchHandler}
-              />
-            }
-          />
-          <Route
-            path="/add"
-            element={<AddContact addContactHandler={addContactHandler} />}
-          />
-          <Route
-            path="/edit"
-            element={
-              <EditContact updateContactHandler={updateContactHandler} />
-            }
-          />
-          <Route path="/contact/:id" element={<ContactDetail />} />
-        </Routes>
+
+        {/* ✅ Responsive Grid Layout */}
+        <div className="ui stackable doubling grid">
+          <div className="row centered">
+            <div className="sixteen wide column">
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <ContactList
+                      contacts={
+                        searchTerm.length < 1 ? contacts : searchResults
+                      }
+                      getContactId={removeContactHandler}
+                      term={searchTerm}
+                      searchHandler={searchHandler}
+                    />
+                  }
+                />
+                <Route
+                  path="/add"
+                  element={<AddContact addContactHandler={addContactHandler} />}
+                />
+                <Route
+                  path="/edit"
+                  element={
+                    <EditContact updateContactHandler={updateContactHandler} />
+                  }
+                />
+                <Route path="/contact/:id" element={<ContactDetail />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
       </BrowserRouter>
     </div>
   );
--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,40 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import user from "../images/user.jpg";
+
 const ContactCard = (props) => {
   let { name, email, id } = props.contact;
+
   return (
     <div className="item">
-      <img className="ui avatar image" src={user} alt="user" />
-      <div className="content" style={{ marginLeft: "10px" }}>
-        <Link to={`/contact/${id}`} state={{ contact: props.contact }}>
-          <div className="header">{name}</div>
-          <div>{email}</div>
-        </Link>
-      </div>
-      <i
-        className="trash alternate outline icon"
-        style={{
-          color: "red",
-          cursor: "pointer",
-          position: "relative",
-          left: "1100px",
-          bottom: "18px",
-        }}
-        onClick={() => props.clickHandler(id)}
-      ></i>
-      <Link to={"/edit"} state={{ contact: props.contact }}>
-        <i
-          className="edit alternate outline icon"
+      <div className="ui grid">
+        {/* Left Section: User Image & Details */}
+        <div
+          className="twelve wide column"
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <img className="ui avatar image" src={user} alt="user" />
+          <div className="content" style={{ marginLeft: "10px" }}>
+            <Link to={`/contact/${id}`} state={{ contact: props.contact }}>
+              <div className="header">{name}</div>
+              <div>{email}</div>
+            </Link>
+          </div>
+        </div>
+
+        {/* Right Section: Icons */}
+        <div
+          className="four wide column"
           style={{
-            color: "green",
-            cursor: "pointer",
-            position: "relative",
-            left: "1050px",
-            bottom: "33px",
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
           }}
-        ></i>
-      </Link>
+        >
+          <i
+            className="trash alternate outline icon red large"
+            style={{ cursor: "pointer", marginRight: "15px" }}
+            onClick={() => props.clickHandler(id)}
+          ></i>
+
+          <Link to={"/edit"} state={{ contact: props.contact }}>
+            <i
+              className="edit alternate outline icon green large"
+              style={{ cursor: "pointer" }}
+            ></i>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
